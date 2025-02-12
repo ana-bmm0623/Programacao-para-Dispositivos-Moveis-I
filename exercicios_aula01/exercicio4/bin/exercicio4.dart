a7e4b13b13d38bc492cf0149142e4a5cf0bf12b5
ana-bmm0623 <anamarques@unitins.br>
2025-02-12 00:04:40 -0300

    exercicio4

---
+++ exercicios_aula01/exercicio4/bin/exercicio4.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:exercicio4/exercicio4.dart' as exercicio4;
+
+void main(List<String> arguments) {
+  stdout.write('Digite um double para se converter em int: ');
+  stdin.readLineSync();
+  var num1 = double.parse(stdin.readLineSync()!);
+  print(
+      'O double $num1 convertido em int é: ${exercicio4.parseDoubleToInt(num1)}');
+  stdout.write('Digite um int para se converter em double: ');
+  stdin.readLineSync();
+  var num2 = int.parse(stdin.readLineSync()!);
+  print(
+    
+      'O int $num2 convertido em double é: ${exercicio4.parseIntToDouble(num2)}');
+}
---
+++ exercicios_aula01/exercicio4/lib/exercicio4.dart
@@ -0,0 +1,7 @@
+int parseDoubleToInt(double a) {
+  return a.toInt();
+}
+
+double parseIntToDouble(int a) {
+  return a.toDouble();
+}
