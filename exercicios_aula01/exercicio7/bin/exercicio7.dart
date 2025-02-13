03c9429c0d1b64087aae3bf3f21b1d655e4fd49d
ana-bmm0623 <anamarques@unitins.br>
2025-02-13 20:33:01 -0300

    exercicio7

---
+++ exercicios_aula01/exercicio7/bin/exercicio7.dart
@@ -0,0 +1,6 @@
+import 'package:exercicio7/exercicio7.dart' as exercicio7;
+
+void main(List<String> arguments) {
+  print('Contagem de 1 a 10:');
+  exercicio7.imprimirAte10();
+}
---
+++ exercicios_aula01/exercicio7/lib/exercicio7.dart
@@ -0,0 +1,5 @@
+void imprimirAte10() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
