475ea70decf0b57e41d73894311546aadf29bbc0
ana-bmm0623 <anamarques@unitins.br>
2025-02-21 10:25:55 -0300

    exercicio 2 da aula03

---
+++ exercicios_aula03/exercicio03/bin/exercicio03.dart
@@ -0,0 +1,5 @@
+import 'package:exercicio03/exercicio03.dart' as exercicio03;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exercicio03.calculate()}!');
+}
---
+++ exercicios_aula03/exercicio03/test/exercicio03_test.dart
@@ -0,0 +1,8 @@
+import 'package:exercicio03/exercicio03.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
