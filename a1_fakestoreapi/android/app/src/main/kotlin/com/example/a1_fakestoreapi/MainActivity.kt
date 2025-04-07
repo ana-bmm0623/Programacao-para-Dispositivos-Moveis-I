f456838fe01c21cf79df858a27e954d04869d49a
ana-bmm0623 <anamarques@unitins.br>
2025-04-06 22:38:54 -0300

    add configuration files and clean up unused assets for Flutter applications

--- a1_fakestoreapi/android/app/src/main/kotlin/com/example/a1_fakestoreapi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.a1_fakestoreapi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ a1_tecnstore/android/app/src/main/kotlin/com/example/a1_tecnstore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a1_tecnstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
